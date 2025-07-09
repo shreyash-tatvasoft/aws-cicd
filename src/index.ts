4506e7e21159cb1cd726651b6dbb31e6938b0e09
shreyash-tatvasoft <shreyash.zala@tatvasoft.com>
2025-07-09 12:09:31 +0530

    Simple Node App

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' }); // Use text/html instead of text/plain
-  res.end('<h1>Welcome to Node application on web server</h1>');
+  res.end('<h1>Welcome to Node application on Web server</h1>');
 });
 
 server.listen(PORT, () => {
