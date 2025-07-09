698c83a47a6673e2a2366d2f6ccc97968fc871ea
shreyash-tatvasoft <shreyash.zala@tatvasoft.com>
2025-07-09 18:14:55 +0530

    Start Server  scripts updated

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,18 +2,20 @@
 set -e
 
 cd /home/ec2-user/myapp
-echo "Current working directory: $(pwd)"
-echo "Listing contents:"
-ls -la
+
+echo "Current directory: $(pwd)"
+echo "Listing files:"
+ls -la dist
 
 echo "Fixing ownership..."
 sudo chown -R ec2-user:ec2-user /home/ec2-user/myapp
 
-if [ ! -f index.js ]; then
-  echo "Error: index.js not found in $(pwd)"
+# Ensure dist/index.js exists
+if [ ! -f dist/index.js ]; then
+  echo "Error: dist/index.js not found!"
   exit 1
 fi
 
 echo "Starting Node.js app..."
-nohup node index.js > app.log 2>&1 &
+nohup node dist/index.js > app.log 2>&1 &
 echo $! > app.pid
