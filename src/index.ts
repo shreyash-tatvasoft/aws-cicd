f74122ce9bf0ea0882df8b68159d2b4997de8a53
shreyash-tatvasoft <shreyash.zala@tatvasoft.com>
2025-07-10 10:51:56 +0530

    PORT CONFIGURE

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import http from 'http';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' }); // Use text/html instead of text/plain
