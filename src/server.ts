56fbcd2f7500b43d51edc9d97f4c1c2e25eb4969
shreyash-tatvasoft <110668075+shreyash-tatvasoft@users.noreply.github.com>
2025-08-13 12:53:22 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     console.log("API request", req, res);
